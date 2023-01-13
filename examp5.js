54bf102cedc44cd1ebb3b7c7d3ed243ab70c7877
vladimir kasperovich <92395305+Vladimirkasperovich@users.noreply.github.com>
2023-01-13 13:41:27 +0300

    lesson5
    lesson5

---
+++ examp5.js
@@ -0,0 +1,50 @@
+// function trueOrFalse(wasThatTrue) {
+//     if (wasThatTrue){
+//       return 'Yes, that was true';
+//     }
+    
+//     return 'No, that was false';
+    
+//     }
+
+/**************************************************** */
+
+// function testEqual(val) {
+//     if (val) { // Change this line
+//       return "Equal";
+//     }
+//     return "Not Equal";
+//   }
+  
+//   testEqual(10);
+/************************************************************ */
+// Setup
+// function testStrict(val) {
+//     if (val === 7) { // Change this line
+//       return "Equal";
+//     }
+//     return "Not Equal";
+//   }
+  
+//   testStrict(10);
+/************************************************** */
+// Setup
+// function compareEquality(a, b) {
+//     if (a === b) { // Change this line
+//       return "Equal";
+//     }
+//     return "Not Equal";
+//   }
+  
+//   compareEquality(10, "10");
+/*************************************************** */
+// Setup
+// function testNotEqual(val) {
+//     if (val != 99) { // Change this line
+//       return "Not Equal";
+//     }
+//     return "Equal";
+//   }
+  
+//   testNotEqual(10);
+/************************************************* */
